9a94657830b522681c799144a1949767c1cb337f
fatur <fatur@inforsys.co.id>
2020-06-08 11:26:20 +0700

    Initial commit

---
+++ OCRTest/MainPage.xaml.cs
@@ -0,0 +1,468 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Media;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Streams;
+using Windows.System.Display;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OCRTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private static readonly Guid RotationKey = new Guid("C380465D-2271-428C-9B83-ECEA3B4A85C1");
+        private readonly SystemMediaTransportControls _systemMediaControls = SystemMediaTransportControls.GetForCurrentView();
+
+        private MediaCapture _mediaCapture;
+        private bool _isPreviewing;
+
+        private bool _isActivePage;
+        private bool _isSuspending;
+        private Task _setupTask = Task.CompletedTask;
+        private bool _isUIActive;
+        private bool _isInitialized;
+        private bool _externalCamera;
+        private bool _mirroringPreview;
+        private CameraRotationHelper _rotationHelper;
+        private readonly DisplayRequest _displayRequest = new DisplayRequest();
+        private StorageFolder _captureFolder = null;
+        public  MainPage()
+        {
+            this.InitializeComponent();
+
+           /* _mediaCapture = new MediaCapture();
+             await _mediaCapture.InitializeAsync();
+            _mediaCapture.Failed += MediaCapture_Failed;
+           */
+        }
+        protected override  async void  OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Application.Current.Suspending += Application_Suspending;
+            Application.Current.Resuming += Application_Resuming;
+            Window.Current.VisibilityChanged += Window_VisibilityChanged;
+
+            _isActivePage = true;
+            await SetUpBasedOnStateAsync();
+        }
+
+        private async Task SetUpBasedOnStateAsync()
+        {
+            // Avoid reentrancy: Wait until nobody else is in this function.
+            while (!_setupTask.IsCompleted)
+            {
+                await _setupTask;
+            }
+
+            // We want our UI to be active if
+            // * We are the current active page.
+            // * The window is visible.
+            // * The app is not suspending.
+            bool wantUIActive = _isActivePage && Window.Current.Visible && !_isSuspending;
+
+            if (_isUIActive != wantUIActive)
+            {
+                _isUIActive = wantUIActive;
+
+                Func<Task> setupAsync = async () =>
+                {
+                    if (wantUIActive)
+                    {
+                        await SetupUiAsync();
+                        await InitializeCameraAsync();
+                    }
+                    else
+                    {
+                        await CleanupCameraAsync();
+                        await CleanupUiAsync();
+                    }
+                };
+                _setupTask = setupAsync();
+            }
+
+            await _setupTask;
+        }
+        private async Task CleanupUiAsync()
+        {
+            UnregisterEventHandlers();
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.None;
+        }
+        private void UnregisterEventHandlers()
+        {
+            _systemMediaControls.PropertyChanged -= SystemMediaControls_PropertyChanged;
+        }
+        private async void SystemMediaControls_PropertyChanged(SystemMediaTransportControls sender, SystemMediaTransportControlsPropertyChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                // Only handle this event if this page is currently being displayed
+                if (args.Property == SystemMediaTransportControlsProperty.SoundLevel && Frame.CurrentSourcePageType == typeof(MainPage))
+                {
+                    // Check to see if the app is being muted. If so, it is being minimized.
+                    // Otherwise if it is not initialized, it is being brought into focus.
+                    if (sender.SoundLevel == SoundLevel.Muted)
+                    {
+                        await CleanupCameraAsync();
+                    }
+                    else if (!_isInitialized)
+                    {
+                        await InitializeCameraAsync();
+                    }
+                }
+            });
+        }
+        private async void Window_VisibilityChanged(object sender, VisibilityChangedEventArgs e)
+        {
+            await SetUpBasedOnStateAsync();
+        }
+
+        private void Application_Resuming(object sender, object e)
+        {
+            _isSuspending = false;
+
+            var task = Dispatcher.RunAsync(CoreDispatcherPriority.High, async () =>
+            {
+                await SetUpBasedOnStateAsync();
+            });
+        }
+
+        private void Application_Suspending(object sender, SuspendingEventArgs e)
+        {
+            _isSuspending = false;
+
+            var deferral = e.SuspendingOperation.GetDeferral();
+            var task = Dispatcher.RunAsync(CoreDispatcherPriority.High, async () =>
+            {
+                await SetUpBasedOnStateAsync();
+                deferral.Complete();
+            });
+        }
+
+        protected override async void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            Application.Current.Suspending -= Application_Suspending;
+            Application.Current.Resuming -= Application_Resuming;
+            Window.Current.VisibilityChanged -= Window_VisibilityChanged;
+
+            _isActivePage = false;
+            await SetUpBasedOnStateAsync();
+        }
+
+
+        private async Task SetupUiAsync()
+        {
+            /*
+            // Attempt to lock page to landscape orientation to prevent the CaptureElement from rotating, as this gives a better experience
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+
+            // Hide the status bar
+            if (ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+            {
+                await Windows.UI.ViewManagement.StatusBar.GetForCurrentView().HideAsync();
+            }
+
+            RegisterEventHandlers();
+            */
+            var picturesLibrary = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Pictures);
+            // Fall back to the local app storage if the Pictures Library is not available
+            _captureFolder = picturesLibrary.SaveFolder ?? ApplicationData.Current.LocalFolder;
+        
+        }
+        private async Task InitializeCameraAsync()
+        {
+            Debug.WriteLine("InitializeCameraAsync");
+
+            if (_mediaCapture == null)
+            {
+                // Attempt to get the back camera if one is available, but use any camera device if not
+                var cameraDevice = await FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel.Back);
+
+                if (cameraDevice == null)
+                {
+                    Debug.WriteLine("No camera device found!");
+                    return;
+                }
+
+                // Create MediaCapture and its settings
+                _mediaCapture = new MediaCapture();
+
+                _mediaCapture.Failed += MediaCapture_Failed;
+
+                var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+
+                // Initialize MediaCapture
+                try
+                {
+                    await _mediaCapture.InitializeAsync(settings);
+                    _isInitialized = true;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Debug.WriteLine("The app was denied access to the camera");
+                }
+
+                // If initialization succeeded, start the preview
+                if (_isInitialized)
+                {
+                    // Figure out where the camera is located
+                    if (cameraDevice.EnclosureLocation == null || cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Unknown)
+                    {
+                        // No information on the location of the camera, assume it's an external camera, not integrated on the device
+                        _externalCamera = true;
+                    }
+                    else
+                    {
+                        // Camera is fixed on the device
+                        _externalCamera = false;
+
+                        // Only mirror the preview if the camera is on the front panel
+                        _mirroringPreview = (cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Front);
+                    }
+
+                    // Initialize rotationHelper
+                    _rotationHelper = new CameraRotationHelper(cameraDevice.EnclosureLocation);
+                    _rotationHelper.OrientationChanged += RotationHelper_OrientationChanged;
+
+                    await StartPreviewAsync();
+
+                    UpdateCaptureControls();
+                }
+            }
+        }
+        private void UpdateCaptureControls()
+        {
+            // The buttons should only be enabled if the preview started sucessfully
+            PhotoButton.IsEnabled = _isPreviewing;
+
+           
+            if (_isInitialized )
+            {
+                PhotoButton.Opacity = PhotoButton.IsEnabled ? 1 : 0;
+            }
+        }
+        private async Task StartPreviewAsync()
+        {
+            // Prevent the device from sleeping while the preview is running
+            _displayRequest.RequestActive();
+
+            // Set the preview source in the UI and mirror it if necessary
+            PreviewControl.Source = _mediaCapture;
+            PreviewControl.FlowDirection = _mirroringPreview ? FlowDirection.RightToLeft : FlowDirection.LeftToRight;
+
+            // Start the preview
+            await _mediaCapture.StartPreviewAsync();
+            _isPreviewing = true;
+
+            // Initialize the preview to the current orientation
+            if (_isPreviewing)
+            {
+                await SetPreviewRotationAsync();
+            }
+        }
+        private async Task SetPreviewRotationAsync()
+        {
+            // Only need to update the orientation if the camera is mounted on the device
+            if (_externalCamera) return;
+
+            // Add rotation metadata to the preview stream to make sure the aspect ratio / dimensions match when rendering and getting preview frames
+            var rotation = _rotationHelper.GetCameraPreviewOrientation();
+            var props = _mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview);
+            props.Properties.Add(RotationKey, CameraRotationHelper.ConvertSimpleOrientationToClockwiseDegrees(rotation));
+            await _mediaCapture.SetEncodingPropertiesAsync(MediaStreamType.VideoPreview, props, null);
+        }
+        private async void MediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            Debug.WriteLine("MediaCapture_Failed: (0x{0:X}) {1}", errorEventArgs.Code, errorEventArgs.Message);
+
+            await CleanupCameraAsync();
+
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => UpdateCaptureControls());
+        }
+
+        private async Task CleanupCameraAsync()
+        {
+            Debug.WriteLine("CleanupCameraAsync");
+
+            if (_isInitialized)
+            {
+               
+                if (_isPreviewing)
+                {
+                    // The call to stop the preview is included here for completeness, but can be
+                    // safely removed if a call to MediaCapture.Dispose() is being made later,
+                    // as the preview will be automatically stopped at that point
+                    await StopPreviewAsync();
+                }
+
+                _isInitialized = false;
+            }
+
+            if (_mediaCapture != null)
+            {
+                _mediaCapture.Failed -= MediaCapture_Failed;
+                _mediaCapture.Dispose();
+                _mediaCapture = null;
+            }
+
+            if (_rotationHelper != null)
+            {
+                _rotationHelper.OrientationChanged -= RotationHelper_OrientationChanged;
+                _rotationHelper = null;
+            }
+        }
+        private async Task StopPreviewAsync()
+        {
+            // Stop the preview
+            _isPreviewing = false;
+            await _mediaCapture.StopPreviewAsync();
+
+            // Use the dispatcher because this method is sometimes called from non-UI threads
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // Cleanup the UI
+                PreviewControl.Source = null;
+
+                // Allow the device screen to sleep now that the preview is stopped
+                _displayRequest.RequestRelease();
+            });
+        }
+        private async void RotationHelper_OrientationChanged(object sender, bool updatePreview)
+        {
+            if (updatePreview)
+            {
+                await SetPreviewRotationAsync();
+            }
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => UpdateButtonOrientation());
+        }
+        private void UpdateButtonOrientation()
+        {
+            // Rotate the buttons in the UI to match the rotation of the device
+            var angle = CameraRotationHelper.ConvertSimpleOrientationToClockwiseDegrees(_rotationHelper.GetUIOrientation());
+            var transform = new RotateTransform { Angle = angle };
+
+            // The RenderTransform is safe to use (i.e. it won't cause layout issues) in this case, because these buttons have a 1:1 aspect ratio
+            PhotoButton.RenderTransform = transform;
+        }
+        private static async Task<DeviceInformation> FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel desiredPanel)
+        {
+            // Get available devices for capturing pictures
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            // Get the desired camera by panel
+            DeviceInformation desiredDevice = allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desiredPanel);
+
+            // If there is no device mounted on the desired panel, return the first device found
+            return desiredDevice ?? allVideoDevices.FirstOrDefault();
+        }
+        private async void PhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            await TakePhotoAsync();
+            await ReadTextWithOCR();
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await ReadTextWithOCR();
+        }
+
+        private async Task ReadTextWithOCR()
+        {
+            var file = await _captureFolder.GetFileAsync("SimplePhoto.jpg");
+            var fileStream = await file.OpenStreamForReadAsync();
+            byte[] byteImg;
+            using (var image = new MagickImage(fileStream))
+            {
+                image.Format = MagickFormat.Tiff;
+                byteImg = image.ToByteArray();
+            }
+
+            using (var engine = new Tesseract.TesseractEngine(@"./tessdata", "eng", Tesseract.EngineMode.Default))
+            {
+                using (var img = Tesseract.Pix.LoadTiffFromMemory(byteImg))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        ResultText.Text = page.GetText();
+                    }
+                }
+            }
+        }
+
+        private async Task TakePhotoAsync()
+        {
+           
+
+            var stream = new InMemoryRandomAccessStream();
+
+            Debug.WriteLine("Taking photo...");
+            await _mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), stream);
+
+            try
+            {
+                var file = await _captureFolder.CreateFileAsync("SimplePhoto.jpg", CreationCollisionOption.ReplaceExisting);
+                Debug.WriteLine("Photo taken! Saving to " + file.Path);
+
+                var photoOrientation = CameraRotationHelper.ConvertSimpleOrientationToPhotoOrientation(_rotationHelper.GetCameraCaptureOrientation());
+
+                await ReencodeAndSavePhotoAsync(stream, file, photoOrientation);
+                Debug.WriteLine("Photo saved!");
+            }
+            catch (Exception ex)
+            {
+                // File I/O errors are reported as exceptions
+                Debug.WriteLine("Exception when taking a photo: " + ex.ToString());
+            }
+
+           
+        }
+        private static async Task ReencodeAndSavePhotoAsync(IRandomAccessStream stream, StorageFile file, PhotoOrientation photoOrientation)
+        {
+            using (var inputStream = stream)
+            {
+                var decoder = await BitmapDecoder.CreateAsync(inputStream);
+
+                using (var outputStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    var encoder = await BitmapEncoder.CreateForTranscodingAsync(outputStream, decoder);
+
+                    var properties = new BitmapPropertySet { { "System.Photo.Orientation", new BitmapTypedValue(photoOrientation, PropertyType.UInt16) } };
+
+                    await encoder.BitmapProperties.SetPropertiesAsync(properties);
+                    await encoder.FlushAsync();
+                }
+            }
+        }
+
+
+        
+    }
+}
